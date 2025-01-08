dfbee2cad79044de29fcd710252ec270e1c5efdd
Adam <85541373+creepicrawli@users.noreply.github.com>
2025-01-08 03:32:14 +0200

    Update WeddingVideo.js

--- src/WeddingVideo.js
+++ src/WeddingVideo.js
@@ -1,7 +1,7 @@
 import ReactPlayer from 'react-player';
 import './App.css';
 import React, { useRef } from 'react';
-const VIDEO_PATH = 'https://drive.google.com/file/d/1uecO6mYIh_FctC_8hlxU18cB3o0MJIMe/view';
+const VIDEO_PATH = 'https://drive.google.com/file/d/1rMhBhchIlfWMe_P3VcoYQchJtV2Kxtho/view';
 function WeddingVideo() {
   const playerRef = useRef(null);
   return (
