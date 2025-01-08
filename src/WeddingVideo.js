74585873347a5e96bb4462224a6bd2865c731c36
Adam <85541373+creepicrawli@users.noreply.github.com>
2025-01-08 12:12:55 +0200

    Update WeddingVideo.js to preload video

--- src/WeddingVideo.js
+++ src/WeddingVideo.js
@@ -1,12 +1,20 @@
 import ReactPlayer from 'react-player';
 import './App.css';
-import React, { useRef } from 'react';
-const VIDEO_PATH = "https://d22sfwj1wctkr8.cloudfront.net/DASaveDate.mp4";
+import React, { useEffect, useRef } from 'react';
+const VIDEO_PATH = 'https://d22sfwj1wctkr8.cloudfront.net/DASaveDate.mp4';
 function WeddingVideo() {
   const playerRef = useRef(null);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      // Trigger loading by initializing the player
+      playerRef.current.getInternalPlayer();
+    }
+  }, []);
+
   return (
     <div className="VideoStyle">
-      <ReactPlayer ref={playerRef} url={VIDEO_PATH} width="203px" controls={true}
+      <ReactPlayer ref={playerRef} url={VIDEO_PATH} width="203px" playing={false} controls={true}
       />
       {/* <ReactPlayer src={'./D&A Save the date HD'} controls={true} onPlay={() => console.log('video is playing')} onPause={() => console.log('video is paused')} />*/}
     </div>
