776e1a8044a38e93008e6e4e10bd133a7c1bbf52
Adam Daniels <Adam Daniels>
2025-07-18 13:03:39 +0200

    styled links and cosmetic link page

--- src/AppRedirect.js
+++ src/AppRedirect.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { StrictMode, useEffect } from 'react';
+import WeddingVideo from './WeddingVideo.js';
 
 export default function AppRedirect() {
     useEffect(() => {
@@ -17,13 +18,28 @@ export default function AppRedirect() {
                 <img src="/LineArt.png" alt="" width="70%" height="120px" />
                 <p className="FalckWedding">Redirecting...</p>
             </header>
-            <main className="App-Body">
-                <p>If you're not redirected, please use the links below:</p>
-                <ul>
-                    <li><a href="https://play.google.com/store/apps/details?id=com.creepicrawli.FalckWedding&pcampaignid=web_share">Download on Android</a></li>
-                    <li><a href="https://apps.apple.com/za/app/falck-wedding/id6747902593">Download on iOS</a></li>
-                </ul>
+            <main className='App-Body'>
+                <StrictMode>
+                    <WeddingVideo />
+                </StrictMode>
             </main>
-        </div>
+            <footer>
+
+                <p>If you're not redirected, please use the links below:</p>
+
+                <ul className='App-Body2' paddingBottom="10">
+                    <div className='links'>
+                        <li width="60%"><a href="https://play.google.com/store/apps/details?id=com.creepicrawli.FalckWedding&pcampaignid=web_share">Download on Android</a></li>
+                        <li><a href="https://apps.apple.com/za/app/falck-wedding/id6747902593">Download on iOS</a></li>
+                    </div>
+                </ul>
+
+
+
+                <StrictMode>
+                    <WeddingVideo />
+                </StrictMode>
+            </footer>
+        </div >
     );
 }
